50d53fdef9937fee7d4b403c340ae4aaee5d3884
Pasindu Hirumal <pasinduhirumal7@gmail.com>
2025-11-30 13:09:15 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
-// App.jsx
 import BirthdaySurprise from "./components/BirthdaySurprise";
 
 function App() {
   return (
-  <div className="text-red-600">Hi im here</div>
-)
+    <BirthdaySurprise />
+  )
 }
 
 export default App;
